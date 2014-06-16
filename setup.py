ca6618c040ddb454c177202fbf523930ca7a7715
Shawn Nock <nock@nocko.se>
2014-06-16 13:04:15 -0400

    Cleanup of setuptools files

--- setup.py
+++ setup.py
@@ -1,16 +1,21 @@
-from setuptools import setup, Extension
+from setuptools import setup, Extension, find_packages
 
 m = Extension('huffman',
               sources = ['huffman/huffman.c','huffman/pyhuffman.c'],
               includes = ['huffman'])
 
-setup (name = 'huffman',
+setup (name = 'ql-demo-json',
        version = '0.1',
-       description = 'This is a wrapper for the Q3A GPL Source Huffman code routines',
        ext_modules = [m],
        packages = ['huffman'],
-       include_package_data = True,
        package_data = {'': ['*.h']},
+       scripts = ['demo.py'],
+       author = "Shawn Nock",
+       author_email = "nock@nocko.se",
+       description = "This package converts Quakelive Demo files in JSON",
+       license = "GPLv3",
+       keywords = "quake quakelive demo dm_73 quakecon",
+       url = "https://aphr.asia/gitweb/?p=qldemo-python.git;a=summary"
 )
 
 
