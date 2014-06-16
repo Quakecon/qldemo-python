658c924c5f998783643517757b96de84dbddb5de
Shawn Nock <nock@nocko.se>
2014-06-16 12:53:59 -0400

    Removes setuptools auto-install

--- setup.py
+++ setup.py
@@ -1,6 +1,3 @@
-from ez_setup import use_setuptools
-use_setuptools()
-
 from setuptools import setup, Extension
 
 m = Extension('huffman',
@@ -10,6 +7,10 @@ m = Extension('huffman',
 setup (name = 'huffman',
        version = '0.1',
        description = 'This is a wrapper for the Q3A GPL Source Huffman code routines',
-       ext_modules = [m])
+       ext_modules = [m],
+       packages = ['huffman'],
+       include_package_data = True,
+       package_data = {'': ['*.h']},
+)
 
 
